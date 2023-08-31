325eb787e9a8b01a0b327423ae2b24816ab948ba
LouiseV98 <143037181+LouiseV98@users.noreply.github.com>
2023-08-31 12:32:33 -0600

    Copiar carpetas
    Probe y utilice la funcion copytree de la lib shutil para copiar las carpetas

--- Practica 2.py
+++ Practica 2.py
@@ -1,7 +1,17 @@
 import os
+import random
+import string
+import shutil
+
+def numero_Aleatorio():
+    return str(random.randint(0,9))
+
+def letra_Aleatoria():
+    return random.choice(string.ascii_uppercase)
 
 ruta_Carpeta = input("Ingrese la ruta de ubicacion de la carpeta: ")
 if os.path.exists(ruta_Carpeta):
-    print("Si existe la ruta")
+    carpeta_Salida = ruta_Carpeta + '_procesado'
+    shutil.copytree(ruta_Carpeta, carpeta_Salida)
 else:
     print("No existe la ruta")
